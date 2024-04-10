f0f9259cd3627801307208542e66ab6de6e73964
Detpatna24 <163991335+Detpatna24@users.noreply.github.com>
2024-04-10 11:16:16 +0530

    Delete java/cms/com/det directory

--- java/cms/com/det/repo/Dashboardrepo.java
+++
@@ -1,9 +0,0 @@
-package cms.com.det.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import cms.com.det.dto.DashboardStudentFormData;
-
-public interface Dashboardrepo extends JpaRepository<DashboardStudentFormData, Long> {
-
-}
--- java/cms/com/det/repo/StudentRepo.java
+++
@@ -1,9 +0,0 @@
-package cms.com.det.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import cms.com.det.dto.Student;
-
-public interface StudentRepo  extends JpaRepository<Student, Long>{
-
-}
